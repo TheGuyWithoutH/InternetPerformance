a69534f7f8bde9e060f4b3af5f2d215af307b4cd
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-11-23 14:14:48 +0100

    Allow fast config change

---
+++ frontend/src/Services/config.js
@@ -0,0 +1,11 @@
+const testBackendConfig = {
+    url: 'http://localhost',
+    port: 3000
+};
+
+const clusterBackend = {
+    url: 'https://iccluster059.iccluster.epfl.ch',
+    port: 3000
+};
+
+export {clusterBackend as backendConfig};
